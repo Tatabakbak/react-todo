90def785861aef7a28bac07c1d06a03b6b4bfc70
Tata <tats.dev@gmail.com>
2019-11-01 00:33:17 +0300

    Updates

---
+++ precache-manifest.18567efc56ebe07a2784d08d5e516f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30bb1f4102f8a9904bc2464c3581defd",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "1756ea133a6719fe9823",
+    "url": "/react-todo/static/css/main.4246c5c4.chunk.css"
+  },
+  {
+    "revision": "6dac15165f8b4a8b628a",
+    "url": "/react-todo/static/js/2.3b3d0947.chunk.js"
+  },
+  {
+    "revision": "1756ea133a6719fe9823",
+    "url": "/react-todo/static/js/main.8fcee209.chunk.js"
+  },
+  {
+    "revision": "db31805f620bdbd554e9",
+    "url": "/react-todo/static/js/runtime-main.cc8f0ef0.js"
+  }
+]);
